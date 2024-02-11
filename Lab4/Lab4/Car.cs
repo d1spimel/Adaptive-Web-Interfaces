1ed88c1399da1719f3a8c6ffa7400ee6a2469456
d1spimel <73076512+d1spimel@users.noreply.github.com>
2024-02-11 15:48:38 +0200

    LR4

---
+++ Lab4/Lab4/Car.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Reflection;
+
+namespace Lab4
+{
+    public class Car
+    {
+        public string Brand { get; set; }
+        private int Year { get; set; }
+        protected string Model { get; set; }
+        internal string Color { get; set; }
+        protected internal double Price { get; set; }
+
+        public Car(string brand, int year, string model, string color, double price)
+        {
+            Brand = brand;
+            Year = year;
+            Model = model;
+            Color = color;
+            Price = price;
+        }
+
+        public void Start()
+        {
+            Console.WriteLine($"The {Brand} {Model} has started.");
+        }
+
+        private void Accelerate()
+        {
+            Console.WriteLine($"The {Brand} {Model} is accelerating.");
+        }
+
+        protected internal void Brake()
+        {
+            Console.WriteLine($"The {Brand} {Model} is braking.");
+        }
+
+        public bool CheckPrice(double threshold)
+        {
+            if (Price > threshold)
+            {
+                Console.WriteLine($"The {Brand} {Model} is expensive.");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"The {Brand} {Model} is affordable.");
+                return false;
+            }
+        }
+    }
+}
---
+++ Lab4/Lab4/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Reflection;
+
+namespace Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car("Toyota", 2022, "Camry", "Red", 25000.0);
+
+            // 2. Продемонструвати роботу з Type і TypeInfo;
+            TypeInfo typeInfo = car.GetType().GetTypeInfo();
+            Type type = typeInfo.AsType();
+            
+            Console.WriteLine($"Full Name: {typeInfo.FullName}, Name: {typeInfo.Name}");
+            Console.WriteLine($"Is Interface: {typeInfo.IsInterface}");
+            Console.WriteLine($"Is Abstract: {typeInfo.IsAbstract}");
+            Console.WriteLine($"IsClass: {typeInfo.IsClass}");
+            Console.WriteLine($"Is Sealed: {typeInfo.IsSealed}");
+            Console.WriteLine($"Assembly: {type.Assembly}");
+            Console.WriteLine($"Attributes: {type.Attributes}");
+
+            // 3. Продемонструвати роботу з MemberInfo;
+            Console.WriteLine("\nMemberInfo: ");
+            MemberInfo[] members = typeInfo.GetMembers();
+            Console.Write("There are {0} members in ", members.GetLength(0));
+            foreach (var member in members)
+            {
+                Console.WriteLine($"{member.MemberType}: {member.Name}, ReflectedType: {member.ReflectedType}, Module: {member.Module}");
+            }
+
+            // 4. Продемонструвати роботу з FieldInfo;
+            Console.WriteLine("\nFieldInfo: ");
+            FieldInfo[] fields = type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+            foreach (var field in fields)
+            {
+                Console.WriteLine($"Field: {field.Name}, Value: {field.GetValue(field.IsStatic ? null : car)}, Type: {field.FieldType}, " +
+                    $"Attributes: {field.Attributes}");
+            }
+
+            // 5. Продемонструвати роботу з MethodInfo. Викликати будь-який метод через Reflection;
+            Console.WriteLine("\nMethodInfo:");
+            MethodInfo[] methods = type.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+            foreach (var method in methods)
+            {
+                Console.WriteLine($"Name: {method.Name}, Attributes: {method.Attributes}, ReturnParameter: {method.ReturnParameter}, " +
+                    $"ReturnType: {method.ReturnType}, IsPublic: {method.IsPublic}");
+            }
+
+            Console.WriteLine();
+
+            MethodInfo start = type.GetMethod("Start");
+            MethodInfo accelerate = type.GetMethod("Accelerate", BindingFlags.NonPublic | BindingFlags.Instance);
+            MethodInfo brake = type.GetMethod("Brake", BindingFlags.NonPublic | BindingFlags.Instance);
+            MethodInfo checkPrice = type.GetMethod("CheckPrice");
+
+            start.Invoke(car, null);
+            accelerate.Invoke(car, null);
+            brake.Invoke(car, null);
+            checkPrice.Invoke(car, new object[] { 20000.0 });
+        }
+    }
+}
